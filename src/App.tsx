85b2b70583db94d8b4f5c36086610144ae643dcc
jackiejones3bb <jackiejones3bb@gmail.com>
2021-07-13 15:29:11 -0400

    installed material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Container } from '@material-ui/core';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+export interface Session {
+  token: string;
+}
+
+
+
+export class App extends Component<{}, Session> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      token: '',
+    }
+  }
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <h1>Hello World</h1>
+    </Container>
+  )
+}
+
 }
 
 export default App;
